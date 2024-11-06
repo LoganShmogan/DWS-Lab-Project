64bad6227e065a9cf8d73bbe2bc40be8eeabdac5
Logan Young <younglogan09@gmail.com>
2024-11-07 10:49:26 +1300

    Fixed typeo

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'nysql'
+    dialect: 'mysql'
 });
 
 module.exports = dbConnection;
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = expresss.Router();
+const router = express.Router();
 
 // Authentication middleware
 const isAuthenticated = (req, res, next) => {
